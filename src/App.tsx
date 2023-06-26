b37619eefb19540b31f84f9df65172f0467f9aff
shaggy611 <sevczik11@gmail.com>
2023-06-26 20:12:38 +0300

    Add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState, useContext } from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme'
 import WordsContext from './context/WordsContext'
 import './App.css'
 import FetchButton from './components/FetchButton'
@@ -11,17 +13,19 @@ function App() {
 
   return (
     <>
-      <WordsContext.Provider
-        value={{ wordPair, setWordPair, loader, setLoader }}>
-        <Menu />
-        {loader && <LinearProgress color='success' />}
+      <ThemeProvider theme={theme}>
+        <WordsContext.Provider
+          value={{ wordPair, setWordPair, loader, setLoader }}>
+          <Menu />
+          {loader && <LinearProgress color='success' />}
 
-        <br />
-        <FetchButton />
-        <br />
-        <div>{wordPair.eng}</div>
-        <div>{wordPair.ukr}</div>
-      </WordsContext.Provider>
+          <br />
+          <FetchButton />
+          <br />
+          <div>{wordPair.eng}</div>
+          <div>{wordPair.ukr}</div>
+        </WordsContext.Provider>
+      </ThemeProvider>
     </>
   )
 }
---
+++ src/theme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+  typography: {
+    button: {
+      fontSize: '1.2rem',
+    },
+  },
+})
+
+export default theme
